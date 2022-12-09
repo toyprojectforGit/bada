b6e3017599fa8393ddbaf872d10766fc2a41a34e
dlrkdmsdkdld@naver.com <dlrkdmsdkdld@naver.com>
2022-12-10 02:19:38 +0900

    feat : 댓글기능 구현

--- bada1_1/app/src/main/java/com/example/bada1/util/Util.kt
+++ bada1_1/app/src/main/java/com/example/bada1/util/Util.kt
@@ -1,5 +1,17 @@
 package com.example.bada1.util
 
+import android.util.Log
+import com.example.bada1.util.util.TAG
+import java.text.SimpleDateFormat
+import java.util.*
+
 object util {
     val TAG = "로그"
+}
+fun parseTimeToHome():String{
+    val tmp = System.currentTimeMillis()
+    val home_dateFormat= SimpleDateFormat("MM.dd.E", Locale("En", "KR"))
+    val parseData=home_dateFormat.format(tmp)
+    Log.d(TAG,"현재 날짜 ${parseData}")
+    return parseData
 }
